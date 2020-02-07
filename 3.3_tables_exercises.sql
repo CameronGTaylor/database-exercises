73e0928ce37c1b76eae95d0817f220ac1a5c77f0
Cameron Taylor <taylor.cameron.g@gmail.com>
2020-02-07 14:49:26 -0600

    Add database exercise 3.3

---
+++ 3.3_tables_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+
+SHOW TABLES;
+
+DESCRIBE employees; #int, date, varchar, enum
+
+# Question 6: salaries
+# Question 7: All tables would contain string except salaries
+# Question 8: dept_employee_latest_date; salaries; titles
+# Question 9: They have Emp No and Dept No both listed in "employees_with_departments" and "dept_emp"
+
+SHOW CREATE TABLE dept_manager;
