6efc683b3e4643815899c00dcd49f37e3d5e1ebb
Cameron Taylor <taylor.cameron.g@gmail.com>
2020-02-11 08:50:22 -0600

    Replace limit exrecisewith correct code

--- 3.5.3_limit_exercises.sql
+++ 3.5.3_limit_exercises.sql
@@ -1,37 +1,13 @@
 USE employees;
 
-#SELECT * FROM employees 
-#	WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC 
+LIMIT 10;
 
-SELECT first_name, last_name, gender FROM employees 
-	WHERE (first_name = 'Irena'
-	OR first_name = 'Vidya' 
-	OR first_name = 'Maya')
-	AND gender = 'M'
-	ORDER BY last_name DESC , first_name DESC ;
-
-SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%';
-
-SELECT first_name, last_name, hire_date FROM employees 
-	WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-
-SELECT first_name, last_name, birth_date FROM employees WHERE birth_date LIKE '%-12-25';
-
-SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%';
-
-SELECT first_name, last_name FROM employees 
-	WHERE last_name LIKE 'E%'
-	OR last_name LIKE '%e';
-	
-SELECT emp_no, first_name, last_name FROM employees 
-	WHERE last_name LIKE 'E%e'
-	ORDER BY emp_no DESC;
-	
 SELECT first_name, last_name, birth_date, hire_date FROM employees 
 	WHERE birth_date LIKE '%-12-25'
 	AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-	ORDER BY birth_date, hire_date DESC ;
-
-SELECT first_name, last_name FROM employees 
-	WHERE last_name LIKE '%q%'
-	AND last_name NOT LIKE '%qu%';
+	ORDER BY birth_date, hire_date DESC 
+	LIMIT 45,5;
+	
+#(Pages-1)*Limit = Offset
